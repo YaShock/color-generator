06376c6bfadd33ebd1d828a77bcf4b5b4aeda151
YaShock <jashok12@gmail.com>
2019-08-31 22:10:01 +0200

    Create AppImage in bin directory

--- scripts/linux/build_appimage.sh
+++ scripts/linux/build_appimage.sh
@@ -2,4 +2,6 @@
 rm -rf ../../AppDir
 ~/linuxdeploy-x86_64.AppImage --appdir=../../AppDir -i ../../dist/icon.png -d color-generator.desktop -e ../../color-generator
 cp ../../design.glade ../../AppDir/usr/bin
-~/appimagetool-x86_64.AppImage ../../AppDir
+mkdir ../../bin
+rm ../../bin/ColorGenerator-x86_64.AppImage
+~/appimagetool-x86_64.AppImage ../../AppDir ../../bin/ColorGenerator-x86_64.AppImage
